56962a6c5aeb6b77a65a31604784e7bede36909a
Murillo Faria <nascimentomurillo24@gmail.com>
2025-07-23 01:06:16 -0300

    Instalação do EFC e criação da classe ClienteContext

---
+++ ClientesApi/Data/ClienteContext.cs
@@ -0,0 +1,14 @@
+﻿using ClientesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientesApi.Data;
+
+public class ClienteContext : DbContext
+{
+    public DbSet<Cliente> Clientes { get; set; }
+
+    public ClienteContext(DbContextOptions<ClienteContext> opts) : base(opts)
+    {
+        
+    }
+}
